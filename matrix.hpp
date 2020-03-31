22d1cce31ae1c27f70c93ce7eaf2ce12cfceab55
Helbis <filippiotrhelbig@gmail.com>
2020-03-31 21:20:28 +0200

    Added some math functions + - =

--- matrix.hpp
+++ matrix.hpp
@@ -12,6 +12,26 @@ class Matrix{
 		Matrix(int r, int c);			//Matrix with random values
 		~Matrix(void);
 
+
+		//Getters
+		int getRow(void);
+		int getCol(void);
+		int get_rawData(void);	
+
+		//Setters
+		void setRow(int num);
+		void setCol(int num);
+
+		//Utility funcs
+		int operator [](int i); 			//Returns a specific part of data
+
+
+		/*
+		For matrix algebra I used:
+
+		https://en.wikipedia.org/wiki/Matrix_(mathematics)
+		*/
+		
 		void operator =(Matrix other);		//Assign to the matrix other one
 		std::string operator <<(void);		//Print matrix row by row
 		Matrix operator +(Matrix other);	//Add to matrix another matrix
@@ -58,11 +78,9 @@ Matrix::Matrix(int r, int c){
 	/* initialize random seed: */
 	srand(time(NULL));	
 
-	for(int i=0; i<cols; i++){
-		for(int j=0; j<rows; j++){
-			//Fill with random numbers from <0, 99>
-			data.push_back(rand() % TOP_LIMIT_OF_NUMBER);
-		}
+	for(int i=0; i<data.size(); i++){
+		//Fill with random numbers from <0, 99>
+		data.push_back(rand() % TOP_LIMIT_OF_NUMBER);
 	}
 
 	data.shrink_to_fit();
@@ -76,37 +94,103 @@ Matrix::~Matrix(void){
 
 //Functions
 void Matrix::operator =(Matrix other){
+	data.clear();	
 
+	rows = other.getRow();
+	cols = other.getCol();
+
+	data = other.get_rawData();
+
+	data.shrink_to_fit();
 }
 
 
 std::string Matrix::operator <<(void){
+	std::string result;
 
+	for(int i=0; i<rows; i++){
+		for(int j=0; j<cols; j++){
+			result.push_back(std::to_string(data[i*cols + j) + ", ");
+		}
+		
+		result.push_back("\n");
+	}
+
+	return result;
 }
 
 
 Matrix Matrix::operator +(Matrix other){
+	Matrix result;
 
+	if(rows == other.getRow() and cols == other.getCols()){
+		//Add element to element
+		for(int i=0; i<data.size(); i++){
+			resultpush_back(data[i] + other[i]);
+		}		
+	}else{
+		//Unable to add matrices
+	}
+
+	result.shrink_to_fit();
+	return result;	
 }
 
 
 Matrix Matrix::operator -(Matrix other){
+	Matrix result;
 
+	if(rows == other.getRow() and cols == other.getCols()){
+		//Subtract element from element
+		for(int i=0; i<data.size(); i++){
+			result.push_back(data[i] - other[i]);
+		}		
+	}else{
+		//Unable to subtract matrices
+	}
+
+	result.shrink_to_fit();
+	return result;	
 }
 
 
 Matrix Matrix::operator *(Matrix other){
+	Matrix result;
 
+	if(rows == other.getCols() and cols == other.getRow()){
+		//Add element to element
+		for(int i=0; i<rows; i++){
+			for(int j=0; j<cols; j++){
+				//Row column rule	
+			}
+		}		
+	}else{
+		//Unable to multiply matrices
+	}
+
+	result.shrink_to_fit();
+	return result;	
 }
 
 
 Matrix Matrix::operator *(int var){
-
+	for(int i=0; i<data.size(); i++){
+		data[i] *= var;
+	}
 }
 
 
 void Matrix::operator +=(Matrix other){
+	if(rows == other.getRow() and cols == other.getCols()){
+		//Add to this matrix
 
+		for(int i=0; i<data.size(); i++){
+			data[i] += other[i];
+		}
+
+	}else{
+		//Do nothing
+	}
 }
 
 
@@ -116,5 +200,5 @@ void Matrix::operator -=(Matrix other){
 
 
 void Matrix::operator *=(Matrix other){
-
+	
 }
