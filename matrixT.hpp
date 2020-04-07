1097ff0fe7f4e61296c17d92f43e86b0f9feb4fd
Helbis <filippiotrhelbig@gmail.com>
2020-04-07 17:49:20 +0200

    Added missing this-> and deleted resize(num)

--- matrixT.hpp
+++ matrixT.hpp
@@ -19,26 +19,25 @@ class MatrixT{
 		MatrixT<T>(void){ this->resize(0, 0); }		//MatrixT 0x0 -> NULL
 		MatrixT<T>(MatrixT<T> &other);					//MatrixT copied from other
 		MatrixT<T>(size_t r, size_t c);				//MatrixT with random values
-		MatrixT<T>(size_t r, size_t c, std::valarray<T> va){ this->resize(r, c); data=va; }
+		MatrixT<T>(size_t r, size_t c, std::valarray<T> va){ this->resize(r, c); this->data=va; }
 		~MatrixT(void){ /* Empty */ }
 
 
 		//Getters
 		std::valarray<T> getRow(size_t i);
 		std::valarray<T> getCol(size_t i);
-		std::valarray<T> &get_rawData(void){ return data; }
-		size_t getRowNum(void){ return rows; }
-		size_t getColNum(void){ return cols; }
-		size_t get_rawSize(void){ return data.size(); }
+		std::valarray<T> &get_rawData(void){ return this->data; }
+		size_t getRowNum(void){ return this->rows; }
+		size_t getColNum(void){ return this->cols; }
+		size_t get_rawSize(void){ return this->data.size(); }
 
 		//Setters
-		void setRowNum(size_t num){ rows = num; }
-		void setColNum(size_t num){ cols = num; }
+		void setRowNum(size_t num){ this->rows = num; }
+		void setColNum(size_t num){ this->cols = num; }
 		void setData(int COMMAND);								//Set data with specific command
-		void setData(size_t i, size_t j, T d){ data[i*cols + j] = d; }	//Set data to specific value
-		void setData(std::valarray<T> va){ data = va; }		//Set data to given valarray
-		void resize(size_t num){ data.resize(num); }
-		void resize(size_t r, size_t c){ data.resize(r*c); rows=r; cols=c; }
+		void setData(size_t i, size_t j, T d){ this->data[i*this->cols + j] = d; }	//Set data to specific value
+		void setData(std::valarray<T> va){ this->data = va; }		//Set data to given valarray
+		void resize(size_t r, size_t c){ this->data.resize(r*c); this->rows=r; this->cols=c; }
 
 
 		/*
@@ -49,38 +48,38 @@ class MatrixT{
 		//Operator overloading
 		void operator =(MatrixT<T> other);		//Assign to the matrix other one
 		void operator =(std::valarray<T> va){ this->resize(va.size()); this->setData(va); }	
-		T &operator [](size_t i){ return data[i]; }	
-		T &operator ()(size_t i, size_t j){ return data[i*cols + j]; }	
+		T &operator [](size_t i){ return this->data[i]; }	
+		T &operator ()(size_t i, size_t j){ return this->data[i*this->cols + j]; }	
 		MatrixT operator +(MatrixT<T> other);	//Add to matrix another matrix
 		MatrixT operator -(MatrixT<T> other);	//Subtract from matrix another matrix
 		MatrixT operator *(MatrixT<T> other);	//Multiply matrix by matrix, give back new matrix
-		void operator *(T var){ data *= var; }	//Multiply matrix by a scalar 
-		void operator +=(MatrixT<T> other){ data += other.get_rawData(); }		//Add to the current matrix other one
-		void operator -=(MatrixT<T> other){ data -= other.get_rawData(); }		//Subtract from matrix other one
+		void operator *(T var){ this->data *= var; }	//Multiply matrix by a scalar 
+		void operator +=(MatrixT<T> other){ this->data += other.get_rawData(); }		//Add to the current matrix other one
+		void operator -=(MatrixT<T> other){ this->data -= other.get_rawData(); }		//Subtract from matrix other one
 		void operator *=(MatrixT<T> other);		//Multiply matrix by the other one
 		void transpose(void);		
 		
 
 		//Math fanctions from valarray
-		void abs(void){ std::abs(data); }
+		void abs(void){ std::abs(this->data); }
 		//Power funcs
-		void exp(void){ std::exp(data); }
-		void log(void){ std::log(data); }
-		void log10(void){ std::log10(data); }
-		void pow(T exp){ std::pow(data, exp); }
-		void pow(std::valarray<T> va_exp){ std::pow(data, va_exp); }
-		void sqrt(void){ std::sqrt(data); }
+		void exp(void){ std::exp(this->data); }
+		void log(void){ std::log(this->data); }
+		void log10(void){ std::log10(this->data); }
+		void pow(T exp){ std::pow(this->data, exp); }
+		void pow(std::valarray<T> va_exp){ std::pow(this->data, va_exp); }
+		void sqrt(void){ std::sqrt(this->data); }
 		//Trig funcs
-		void sin(void){ std::sin(data); }
-		void cos(void){ std::cos(data); }
-		void tan(void){ std::tan(data); }
-		void asin(void){ std::asin(data); }
-		void acos(void){ std::acos(data); }
-		void atan(void){ std::atan(data); }
-		void atan2(std::valarray<T> va){ std::atan2(data, va); }
-		void sinh(void){ std::sinh(data); }
-		void cosh(void){ std::cosh(data); }
-		void tanh(void){ std::tanh(data); }
+		void sin(void){ std::sin(this->data); }
+		void cos(void){ std::cos(this->data); }
+		void tan(void){ std::tan(this->data); }
+		void asin(void){ std::asin(this->data); }
+		void acos(void){ std::acos(this->data); }
+		void atan(void){ std::atan(this->data); }
+		void atan2(std::valarray<T> va){ std::atan2(this->data, va); }
+		void sinh(void){ std::sinh(this->data); }
+		void cosh(void){ std::cosh(this->data); }
+		void tanh(void){ std::tanh(this->data); }
 			
 
 		//Friends of MatrixT
@@ -107,7 +106,7 @@ MatrixT<T>::MatrixT(size_t r, size_t c){
 
 	srand(time(NULL));	// initialize random seed
 
-	for(size_t i=0; i<data.size(); i++){
+	for(size_t i=0; i<this->get_rawSize(); i++){
 		//Fill with random numbers from <0, 99>
 		data[i] = rand() % TOP_LIMIT_OF_NUMBER;
 	}
@@ -117,10 +116,10 @@ MatrixT<T>::MatrixT(size_t r, size_t c){
 //Getters
 template<typename T>
 std::valarray<T> MatrixT<T>::getRow(size_t i){
-	std::valarray<T> result(cols);
+	std::valarray<T> result(this->cols);
 	
-	for(size_t j=0; j<cols; j++){
-		result[j] = data[i*cols + j];
+	for(size_t j=0; j<this->cols; j++){
+		result[j] = this->data[i*this->cols + j];
 	}
 
 	return result;
@@ -129,10 +128,10 @@ std::valarray<T> MatrixT<T>::getRow(size_t i){
 
 template<typename T>
 std::valarray<T> MatrixT<T>::getCol(size_t j){
-	std::valarray<T> result(rows);
+	std::valarray<T> result(this->rows);
 	
-	for(size_t i=0; i<rows; i++){
-		result[i] = data[i*cols + j];
+	for(size_t i=0; i<this->rows; i++){
+		result[i] = this->data[i*this->cols + j];
 	}
 	
 	return result;
@@ -143,7 +142,6 @@ std::valarray<T> MatrixT<T>::getCol(size_t j){
 template<typename T>
 void MatrixT<T>::setData(int COMMAND){
 	if(COMMAND == CLEAR){
-		this->resize(0);
 		this->resize(0, 0);
 	}
 }
@@ -153,7 +151,7 @@ void MatrixT<T>::setData(int COMMAND){
 template<typename T>
 void MatrixT<T>::operator =(MatrixT<T> other){
 	this->resize(other.getRowNum(), other.getColNum());
-	data = other.get_rawData();
+	this->data = other.get_rawData();
 }
 
 
@@ -161,7 +159,7 @@ template<typename T>
 MatrixT<T> MatrixT<T>::operator +(MatrixT<T> other){
 	MatrixT<T> result;
 
-	if(rows == other.getRowNum() and cols == other.getColNum()){
+	if(this->rows == other.getRowNum() and this->cols == other.getColNum()){
 		//Add element to element
 		result = this->data + other.get_rawData();	
 	}else{
@@ -176,7 +174,7 @@ template<typename T>
 MatrixT<T> MatrixT<T>::operator -(MatrixT<T> other){
 	MatrixT<T> result;
 
-	if(rows == other.getRowNum() and cols == other.getColNum()){
+	if(this->rows == other.getRowNum() and this->cols == other.getColNum()){
 		//Subtract element from element
 		result = this->data - other.get_rawData();	
 	}else{
@@ -191,7 +189,7 @@ template<typename T>
 MatrixT<T> MatrixT<T>::operator *(MatrixT<T> other){
 	MatrixT<T> result;
 
-	if(cols == other.getRowNum()){
+	if(this->cols == other.getRowNum()){
 		result.resize(rows, other.getColNum());
 
 		for(size_t i=0; i<this->getRowNum(); i++){
@@ -209,10 +207,10 @@ MatrixT<T> MatrixT<T>::operator *(MatrixT<T> other){
 
 template<typename T>
 void MatrixT<T>::operator *=(MatrixT<T> other){
-	MatrixT result;
+	MatrixT<T> result;
 
-	if(cols == other.getRowNum()){
-		result.resize(rows, other.getColNum());
+	if(this->cols == other.getRowNum()){
+		result.resize(this->rows, other.getColNum());
 
 		for(size_t i=0; i<this->getRowNum(); i++){
 			for(size_t j=0; j<other.getColNum(); j++){
